cdee0825bf5568e9a649d0dc9eb2034d6f924721
luke <ghlee@fadutec.com>
2019-07-03 20:25:54 +0900

    run_test.sh 변경

--- scripts/spor/2.spor_w_write/run_test.sh
+++ scripts/spor/2.spor_w_write/run_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 PORT=$1
 FORMAT=$2
-DATE=`date +%Y-%m-%d`
-./run_chamber PORT FORMAT | tee >(ts > ./testlog/$DATE.log)
+DATE=`date +%Y-%m-%d_%H_%M`
+./run_chamber $PORT $FORMAT | tee >(ts > test_log/$DATE.log)
